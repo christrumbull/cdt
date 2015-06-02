498fc0e6b93167a2fcfecbf11634b1836f6db53e
christrumbull <chris.trumbull@gmail.com>
2015-06-02 18:08:09 -0400

    Just Testing

--- wp-content/themes/cdt/searchform.php
+++ wp-content/themes/cdt/searchform.php
@@ -1,3 +1,4 @@
+<!-- just making a commite -->
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
         <label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
